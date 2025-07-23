0e2d22c89ead574026fd3fe7174646beb12a7e07
Muhammad Rasikh Riaz <mo.rasikhriaz@gmail.com>
2025-07-23 14:51:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes RASIKH RIAZ );
+  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes RASIKH RIAZ!! );
 });
 
 app.listen(port, () => {
